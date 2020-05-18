77399e186258ca740186c85ab78c5ec35d479c64
Joshua Peavler <joshuaapeavler@gmail.com>
2020-05-18 10:40:28 -0600

    Setup Utilization of cors, dotenv, and mongodb

--- app.js
+++ app.js
@@ -3,9 +3,12 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+require('dotenv').config();
+const cors = require('cors');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+//let schedulerRouter = require('./routes/shedule');
 
 var app = express();
 
@@ -18,9 +21,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+//app.use('/api/scheduler', schedulerRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
