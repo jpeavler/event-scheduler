be81375040e32cfe0066b9b520bb2b8b5d08432b
Joshua Peavler <joshuaapeavler@gmail.com>
2020-05-18 11:10:38 -0600

    Implemented read by id function in data access layer

--- dal/scheduler.js
+++ dal/scheduler.js
@@ -32,3 +32,34 @@ const getEvents = () => {
     });
     return myPromise;
 }
+const getEventById = () => {
+    const myPromise = new Promise((resolve, reject) => {
+        MongoClient.connect(url, settings, function(err, client) {
+            if(err) {
+                reject(err);
+            }else {
+                console.log("Connected to DB for READ by ID");
+                const db = client.db(dbName);
+                const collection = db.collection(colName);
+                try {
+                    const _id = new ObjectID(id);
+                    const result = await collection.findOne({_id});
+                    if(result) {
+                        resolve(result);
+                    }else {
+                        reject({error: "ID not found in database"});
+                    }
+                    client.close();
+                } catch(err) {
+                    reject({error: "ID must be in ObjectID format"});
+                }
+            }
+        });
+    });
+    return myPromise;
+}
+
+module.exports = {
+    getEvents,
+    getEventById
+}
