f8e1028a329c461463d0ab33f869233369984264
Joshua Peavler <joshuaapeavler@gmail.com>
2020-05-18 11:53:44 -0600

    Implemented deleteEvent function in data access layer

--- dal/scheduler.js
+++ dal/scheduler.js
@@ -146,10 +146,43 @@ const updateEventValues = (id, myEvent) => {
     return myPromise;
 }
 
+//DELETE function
+const deleteEvent = (id) => {
+    const myPromise = new Promise((resolve, reject) => {
+        MongoClient.connect(url, settings, async function(err, client) {
+            if(err) {
+                reject(err);
+            } else {
+                console.log("Connected to DB for DELETE");
+                const db = client.db(dbName);
+                const collection = db.collection(colName);
+                try {
+                    const _id = new ObjectID(id);
+                    collection.findOneAndDelete({_id}, function (err, data) {
+                        if(err) {
+                            reject(err);
+                        }else{
+                            if(data.lastErrorObject.n > 0) {
+                                resolve(data.value);
+                            }else{
+                                resolve({error: "ID doesn't exist in Database"})
+                            }
+                        }
+                    });
+                } catch(err) {
+                    reject({error: "ID has to be in ObjectID format"});
+                }
+            }
+        });
+    });
+    return myPromise;
+}
+
 module.exports = {
     getEvents,
     getEventById,
     addEvent,
     updateEvent,
-    updateEventValues
+    updateEventValues,
+    deleteEvent
 }
